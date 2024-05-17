bc0b7b2c9f1d72d3d05b352e7e22031bfc4501f3
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-05-17 09:25:06 +0200

    devendoring deps

--- guix/bench-in-situ.scm
+++ guix/bench-in-situ.scm
@@ -10,15 +10,16 @@
   #:use-module (gnu packages serialization)
   #:use-module (gnu packages pkg-config)
   #:use-module (guix-hpc packages pdi)
-  #:use-module (guix-hpc packages utils))
+  #:use-module (guix-hpc packages utils)
+  #:use-module (gnu packages linux))
 
 (define location
-  (pk 'loc (current-source-directory)))
+  (current-source-directory))
 
 (define-public local-source
   (local-file ".." "source"
               #:recursive? #t
-              #:select? (git-predicate (pk 'l (dirname location)))))
+              #:select? (git-predicate (dirname location))))
 
 (define-public bench-in-situ
   (package
@@ -27,11 +28,27 @@
     (source local-source)
     (build-system cmake-build-system)
     (arguments
-      (list
-        #:tests? #f
-        #:configure-flags #~(list "-DSESSION=MPI_SESSION"
-                                  "-DKokkos_ENABLE_OPENMP=ON"
-                                  "-DEuler_ENABLE_PDI=ON")))
+      `(#:tests? #f
+        #:configure-flags ,#~(list "-DSESSION=MPI_SESSION"
+                                   "-DKokkos_ENABLE_OPENMP=ON"
+                                   "-DEuler_ENABLE_PDI=ON")
+        #:validate-runpath? #f
+        #:phases (modify-phases %standard-phases
+                  (add-after 'unpack 'unvendor
+                    (lambda* (#:key outputs inputs #:allow-other-keys)
+                      (let* ((out (assoc-ref outputs "out"))
+                             (inih (assoc-ref inputs "libinih")))
+                        (begin
+                          (pk inputs)
+                          (pk outputs))))))))
+    ;; FIXME
+                          ;; (pk 'inih inih)
+                          ;; (delete-file-recursively "lib")
+                          ;; (mkdir "lib")
+                          ;; (symlink inih "lib/inih")
+                          ;; (pk "done"))))))))
+                          ;;;
+                          ;; (exit 1))))))))
     (native-inputs (list paraconf
                          pkg-config))
     (inputs (list openmpi
@@ -40,7 +57,8 @@
                   pdiplugin-user-code
                   pdiplugin-decl-hdf5-parallel
                   kokkos
-                  libyaml))
+                  libyaml
+                  libinih))
     (synopsis "")
     (description "")
     (home-page "")

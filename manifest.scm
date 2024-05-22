c849e51073fdd56d851d0068a72e790ffd9d10e5
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-05-22 13:24:00 +0200

    add stdenv

--- manifest.scm
+++ manifest.scm
@@ -1,11 +1,24 @@
+(use-modules (guix build-system gnu)
+             (ice-9 match))
+
+(define stdenv
+  (map
+    (lambda* (pkg)
+      (match pkg
+        ((_ value _ ...)
+         value)))
+    (standard-packages)))
+
 (concatenate-manifests
   (list
+    (packages->manifest stdenv)
     (specifications->manifest
       (list
+        "bash"
         "gcc-toolchain"
+
         "cmake"
-        "make"
-        "openmpi"
+        "openmpi@4"
         "pdi"
         "pdiplugin-mpi"
         "pdiplugin-user-code"
